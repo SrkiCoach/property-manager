5e629e0e0000911b2dd5e05e2e481dbce10650ae
Srdjan Milosavljevic <146416974+SrkiCoach@users.noreply.github.com>
2026-07-08 10:54:50 +0000

    Add backend and frontend projects

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,31 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
+
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+
+    provideTranslateService({
+      lang: 'en',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json'
+      })
+    })
+  ]
+};
---
+++ frontend/src/app/app.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateService, TranslatePipe } from '@ngx-translate/core';
+
+interface HealthResponse {
+  status: string;
+  application: string;
+  version: string;
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [TranslatePipe],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App implements OnInit {
+  protected readonly title = signal('frontend');
+  health = signal<HealthResponse | null>(null);
+
+constructor(
+  private http: HttpClient,
+  private translate: TranslateService
+) {
+  this.translate.addLangs(['en', 'el']);
+  this.translate.setFallbackLang('en');
+  this.translate.use('en');
+}
+
+  ngOnInit(): void {
+    this.http.get<HealthResponse>('/api/health')
+      .subscribe(response => {
+        this.health.set(response);
+      });
+  }
+
+  changeLanguage(language: string): void {
+    this.translate.use(language);
+  }
+}
