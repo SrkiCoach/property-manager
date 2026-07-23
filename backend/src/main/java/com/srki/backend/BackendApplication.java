f1436d239ee82daf4c14e95a5403bba11998147c
ORCO\milosavlievits.s <sergian@or-co.gr>
2026-07-23 12:29:57 +0300

    Migrate backend to PostgreSQL

--- backend/src/main/java/com/srki/backend/BackendApplication.java
+++ backend/src/main/java/com/srki/backend/BackendApplication.java
@@ -1,11 +1,12 @@
 package com.srki.backend;
 
-import com.srki.backend.entity.Customer;
-import com.srki.backend.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.srki.backend.entity.Customer;
+import com.srki.backend.repository.CustomerRepository;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -17,9 +18,11 @@ public class BackendApplication {
     @Bean
     CommandLineRunner initData(CustomerRepository customerRepository) {
         return args -> {
-            customerRepository.save(new Customer("Nikos", "Papadopoulos", "nikos@example.com", "6900000001"));
-            customerRepository.save(new Customer("Maria", "Ioannou", "maria@example.com", "6900000002"));
-            customerRepository.save(new Customer("Giorgos", "Kostas", "giorgos@example.com", "6900000003"));
+            if (customerRepository.count() == 0) {
+                customerRepository.save(new Customer("Nikos", "Papadopoulos", "nikos@example.com", "6900000001"));
+                customerRepository.save(new Customer("Maria", "Ioannou", "maria@example.com", "6900000002"));
+                customerRepository.save(new Customer("Giorgos", "Kostas", "giorgos@example.com", "6900000003"));
+            }
         };
     }
 }
